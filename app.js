5dc1e3fa9e23f27b2b33716d7e0e54ea16158246
Cody Chan <int64ago@gmail.com>
2015-12-24 19:34:14 +0800

    Enable simple authentication

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 var express = require('express'),
+    bodyParser = require('body-parser'),
     cors = require('cors'),
     config = require('./config'),
     op = require('./op'),
     app = express();
 
-app.use(cors(config.cors));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/uptoken', function(req, res) {
-    res.json({
-        token: op.getUpToken(),
-        domain: config.qiniu.domain
-    });
+	if (req.body.getlink_key && req.body.getlink_key === config.AUTH_KEY) {
+	    res.json({
+	        token: op.getUpToken(),
+	        domain: config.qiniu.domain
+	    });
+	} else {
+		res.json({
+			err: 'Unauthorized!'
+		});
+	}
 });
 
 app.listen(process.env.VCAP_APP_PORT || 3000);
--- config.js
+++ config.js
@@ -1,12 +1,12 @@
+var AUTH_KEY = process.env.AUTH_KEY || '<AUTH_KEY>'
+
 var qiniu = {
     accessKey: process.env.ACCESS_KEY || '<ACCESS_KEY>',
     secretKey: process.env.SECRET_KEY || '<SECRET_KEY>',
     bucket: process.env.BUCKET || '<BUCKET>',
-    domain: process.env.DOMAIN || '<DOMAIN>'
+    domain: process.env.DOMAIN || '<DOMAIN>', // eg: https://nzsg3jhu3.qnssl.com
+
 };
 
-var cors = {
-    origin: 'https://get-link.xyz'
-}
-
 exports.qiniu = qiniu;
+exports.AUTH_KEY = AUTH_KEY;
